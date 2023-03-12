214a598caa32ff983ca192248c2274edd6807567
Bj'z <zbjarchi@gmail.com>
2023-03-12 17:30:32 +0200

    :sparkles: Add features
    defines a route at '/api/whoami' that returns a JSON response

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-// index.js
-// where your node app starts
-
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+let cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// This API point defines a route at '/api/whoami' that returns a JSON response with the client's IP address, language and user agent information.
+
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip, // The IP address of the client making the request
+    language: req.headers["accept-language"], // The language accepted by the client
+    software: req.headers["user-agent"], // The client's user agent information
+  });
+});
+
+// To 404
+app.use((req, res) => {
+  res.status(404).sendFile(path.resolve("views/404.html"));
+});
+
+// listen for requests on port: 3000
+letlistener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
